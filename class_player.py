b7d40c9051bf2a8b00029437b0a834775ee1c9ac
yjb23tech <yjb23tech@gmail.com>
2023-10-02 00:06:25 +0100

    Sticky

--- class_player.py
+++ class_player.py
@@ -2,14 +2,20 @@ import random
 
 class Player:
 
-    def __init__(self, str_name):
+    def __init__(self, str_name, str_birth_place, int_age):
 
         self.str_name = str_name
+        self.str_birth_place = str_birth_place
+        self.int_age = int_age 
+
         self.int_atk_pwr = 1
-        
+
         self.int_loc_x = 1 
         self.int_loc_y = 1
     
+    def __str__(self):
+        return (f"My name is {self.str_name} and I'm going to be the Pirate King XD")
+
     def int_launch_atk(self):
         self.int_atk_pwr = random.randint(15,25)
         return self.int_atk_pwr 
--- treasure_tiles.py
+++ treasure_tiles.py
@@ -1,5 +1,6 @@
 from class_player import Player
 from data_structures import arr_world_map, arr_tile_bosses, arr_options 
+from funcs import str_set_player_name, str_set_player_birth_place, int_set_player_age 
 
 
 
